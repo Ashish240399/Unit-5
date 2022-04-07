51379134b2fbf7d393bb66bd1daea8e1e7f6d8ec
Ashish <ashishkumarmohanty@gmail.com>
2022-04-07 23:37:02 +0530

    nav

---
+++ navbar/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="nav">
+        <div><b>LOGOBAKERY</b></div>
+        <div>
+          <span>Services</span>
+          <span>Projects</span>
+          <span>About</span>
+        </div>
+        <div>
+          <button class="btn">Contact</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+// const button=document.querySelector(".btn");
+// button.addEventListener("click",()=>{
+//   alert ("complete it");
+// });
+export default App;
