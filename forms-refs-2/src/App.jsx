d3f17f57e15eac4f001368d102a0cad186f00abd
Ashish <ashishkumarmohanty@gmail.com>
2022-04-18 09:16:23 +0530

    commit

---
+++ forms-refs-2/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Form } from './components/form';
+import { Table } from './components/table';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
