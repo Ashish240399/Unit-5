49e1e00f2299332a6a967f36edb7ddd218f00ba4
Ashish <ashishkumarmohanty@gmail.com>
2022-04-27 16:53:58 +0530

    Pushing

--- toggle-api/src/App.jsx
+++ toggle-api/src/App.jsx
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import CustomizedSwitches from "./components/toggle"
-
+import './App.css'
+import {Body} from "./componants/body"
+import {H1} from "./componants/h1"
+import {Img} from './componants/img'
+import {Navbar} from "./componants/navbar"
+import {useContext} from 'react'
+import {ThemeContext} from "./contexts/themeContext"
 function App() {
-  
+  const {theme}=useContext(ThemeContext)
+
   return (
     <div className="App">
-      <CustomizedSwitches/>
+      <Navbar/>
+        <Body id={theme} >
+          <Img src='https://qwesys.com/wp-content/uploads/2020/02/web.gif'/>
+          <H1 id={theme}>Web Developer</H1>
+        </Body>
+        
+          
+           
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
